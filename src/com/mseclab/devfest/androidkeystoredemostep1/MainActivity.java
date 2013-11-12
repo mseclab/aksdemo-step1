c4d06f4e9d240d96cb7fcb7d419deeb4e6dd8ade
r.piccirillo <r.piccirillo@mseclab.com>
2013-11-12 18:03:02 +0100

    Generazione chiavi

--- src/com/mseclab/devfest/androidkeystoredemostep1/MainActivity.java
+++ src/com/mseclab/devfest/androidkeystoredemostep1/MainActivity.java
@@ -1,10 +1,23 @@
 package com.mseclab.devfest.androidkeystoredemostep1;
 
+import java.math.BigInteger;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.util.Calendar;
+
+import javax.security.auth.x500.X500Principal;
+
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.security.KeyPairGeneratorSpec;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -17,86 +30,133 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	private final static String ALIAS = "DEVKEY1";
 
-        if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+		if (savedInstanceState == null) {
+			getFragmentManager().beginTransaction()
+					.add(R.id.container, new PlaceholderFragment()).commit();
+		}
+	}
 
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        switch (item.getItemId()) {
-            case R.id.action_settings:
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment implements View.OnClickListener {
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		switch (item.getItemId()) {
+		case R.id.action_settings:
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 
-        private Button mGenChiaviButton;
-        private TextView mDebugText;
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment implements
+			View.OnClickListener {
 
-        private static final String TAG = "AndroidKeyStoreDemo";
+		private Button mGenChiaviButton;
+		private TextView mDebugText;
+		ProgressDialog progressdialog;
 
-        public PlaceholderFragment() {
-        }
+		private static final String TAG = "AndroidKeyStoreDemo";
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            mGenChiaviButton = (Button) rootView.findViewById(R.id.generate_button);
-            mGenChiaviButton.setOnClickListener(this);
-            mDebugText = (TextView) rootView.findViewById(R.id.debugText);
+		public PlaceholderFragment() {
+		}
 
-            return rootView;
-        }
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_main, container,
+					false);
+			mGenChiaviButton = (Button) rootView
+					.findViewById(R.id.generate_button);
+			mGenChiaviButton.setOnClickListener(this);
+			mDebugText = (TextView) rootView.findViewById(R.id.debugText);
 
-        @Override
-        public void onClick(View view) {
+			return rootView;
+		}
 
-            switch (view.getId()) {
-                case R.id.generate_button:
-                    debug("Cliccato Genera chivi");
-                    generaChiavi();
-                    break;
-            }
-        }
+		@Override
+		public void onClick(View view) {
 
-        private void generaChiavi() {
-            AsyncTask<Void, Void, Void> generaTask = new AsyncTask<Void, Void, Void>() {
-                @Override
-                protected Void doInBackground(Void... voids) {
-                    return null;
-                }
-            }.execute();
-        }
+			switch (view.getId()) {
+			case R.id.generate_button:
+				debug("Cliccato Genera chivi");
+				generaChiavi();
+				break;
+			}
+		}
 
-        private void debug(String message){
-            mDebugText.append(message + "\n");
-            Log.v(TAG, message);
-        }
-    }
+		private void generaChiavi() {
+
+			Context cx = getActivity();
+			// Generate a key pair inside the AndroidKeyStore
+			Calendar notBefore = Calendar.getInstance();
+			Calendar notAfter = Calendar.getInstance();
+			notAfter.add(1, Calendar.YEAR);
+
+			android.security.KeyPairGeneratorSpec.Builder builder = new KeyPairGeneratorSpec.Builder(
+					cx);
+			builder.setAlias(ALIAS);
+			String infocert = String.format("CN=%s, OU=%s", ALIAS,
+					cx.getPackageName());
+			builder.setSubject(new X500Principal(infocert));
+			builder.setSerialNumber(BigInteger.ONE);
+			builder.setStartDate(notBefore.getTime());
+			builder.setEndDate(notAfter.getTime());
+			KeyPairGeneratorSpec spec = builder.build();
+
+			KeyPairGenerator kpGenerator;
+			KeyPair kp = null;
+			try {
+				//ShowGenerating();
+				progressdialog = ProgressDialog.show(this.getActivity(), "Progress Dialog","Generating keys....",true);
+				kpGenerator = KeyPairGenerator.getInstance("RSA",
+						"AndroidKeyStore");
+				kpGenerator.initialize(spec);
+				kp = kpGenerator.generateKeyPair();
+				debug("Generated key pair:" + kp.toString());
+				progressdialog.dismiss();
+
+			} catch (NoSuchAlgorithmException e2) {
+				debug(e2.toString());
+			} catch (NoSuchProviderException e2) {
+				debug(e2.toString());
+			} catch (InvalidAlgorithmParameterException e) {
+				debug(e.toString());
+			}
+
+			
+
+		}
+
+		private void ShowGenerating() {
+			progressdialog = new ProgressDialog(this.getActivity());
+			progressdialog.setTitle("Generating keys....");
+			progressdialog.setIndeterminate(false);
+			progressdialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+			progressdialog.show();
+		}
+
+		public void debug(String message) {
+			mDebugText.append(message + "\n");
+			Log.v(TAG, message);
+		}
+	}
 
 }
